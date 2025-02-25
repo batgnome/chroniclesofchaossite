0ede8de95b167e77a2223bb823d1b8c94a5a5962
batgnome <fouflord@gmail.com>
2025-02-24 20:56:00 -0700

    use hash routing instead of default

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
