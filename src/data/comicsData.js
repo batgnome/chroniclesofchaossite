f42b48130a4060ea9cae1d17b325de48cb896e62
batgnome <fouflord@gmail.com>
2025-03-01 20:48:50 -0700

    add title card

--- src/data/comicsData.js
+++ src/data/comicsData.js
@@ -3,14 +3,14 @@ const comics = [
       id: "chronicles-of-chaos",
       title: "Chronicles of Chaos",
       description: "Follow the adventures of Benny and Fred as they navigate a strange world.",
-      image: "https://example.com/comic-thumbnail.jpg",
-    },
-    {
-      id: "junkyard-sorceress",
-      title: "Junkyard Sorceress",
-      description: "A redneck mechanic girl accidentally summons a mischievous demon in her junkyard.",
-      image: "https://example.com/junkyard-thumbnail.jpg",
+      image: "/TitleCards/ChroniclesOfChaosTitleCard.png",
     }
+    // ,{
+    //   id: "junkyard-sorceress",
+    //   title: "Junkyard Sorceress",
+    //   description: "A redneck mechanic girl accidentally summons a mischievous demon in her junkyard.",
+    //   image: "https://example.com/junkyard-thumbnail.jpg",
+    // }
   ];
   
   export default comics;
