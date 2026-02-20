69590b54232c94901d234e5e35597efebfe89fa4
elias hawkins <fouflord@gmail.com>
2026-02-20 14:05:19 -0700

    unused 4

--- src/components/ComicReader.js
+++ src/components/ComicReader.js
@@ -1,11 +1,11 @@
-// import { useParams, useSearchParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import { useRef, useState } from "react";
 import HTMLFlipBook from "react-pageflip";
 import comics from "../data/comicsData";
 
 function ComicReader() {
   const totalPages = 61; // Adjust based on your episodes
-  // const { id } = useParams();
+  const { id } = useParams();
   const comic = comics.find((c) => c.id === id); // 👈 look up the comic by ID
   const bookRef = useRef();
   const [currentPage, setCurrentPage] = useState(0);
